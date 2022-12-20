b0086a8cb4c73c6dabfc45cfda1b19f048fd43df
DeAdrien Hill <deadrienhill@yahoo.com>
2022-12-20 12:49:54 -0600

    created video game list and ordered it by video game name length

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace LINQ
+﻿using System;
+using LINQ;
+using System.Collections.Generic;
+
+
+namespace LINQ
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<string> videoGames = new List<string>
+            {"Call of Duty Modern Warfare 2", "NFL Madden 2023", "NBA 2K 2023", "Call of The Wild", "Farming Simulator 2022" };
+
+            IEnumerable<string> lengthVideoGameNames = videoGames.OrderBy(name => name.Length);
+
+            foreach (var name in lengthVideoGameNames)
+            {
+                Console.WriteLine(name);
+            }
+
+                
+
+            
+            
+               
+            
         }
     }
 }
